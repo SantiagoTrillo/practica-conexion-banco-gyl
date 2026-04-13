89a29e64248e2401749f0b660a590697e6580987
leonardolscuccimarra <leonardolscuccimarra@gmail.com>
2026-04-13 20:23:19 -0300

    WIP 0.2 Se crea clase Sucursal

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -1,33 +1,39 @@
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 //Esta clase simula datos persistentes históricos a la fecha de inicializar el proyecto
 //Todas estas inyecciones son hard-codeadas a modo de ejemplo
 
 public class DataBase {
     public void DataBase(){
-        cargarSucursales();
-        cargarAdmin();
-        cargarClientes();
+        Sucursal objBanco = cargarUnaSucursal();
+        //cargarSucursales();
+        //cargarAdmin();
+        cargarClientes(objBanco.registro);
     }
 
+    private Sucursal cargarUnaSucursal(){
+        return new Sucursal("Central","Calle Central 5","14-04-1998", new RegistroClientela());
+    }
     private void cargarSucursales(){
-        new Sucursal.Builder()
+        List<Sucursal> sucursalList = new ArrayList<>();
     }
 
-    private void cargarClientes() {
+    private void cargarClientes(InterfaceClientela registro) {
         new Cliente.Builder("juanperez27", "juanelmaskpo","Juan","Perez","Calle Falsa 123")
-                .build();
+                .build(registro);
         new Cliente.Builder("ahorrista", "colchon","Joni","Gasto", "Calle Barata 123")
                 .tipoCuenta("Ahorro")
-                .build();
+                .build(registro);
         new Cliente.Builder("cashdollar", "$$$$$$$$","Ricky", "Ricon", "Puerto Madero 1234")
                 .tipoCuenta("Black")
                 .saldo(BigDecimal.valueOf(999999999))
-                .build();
+                .build(registro);
         new Cliente.Builder("islascaimansa", "islascaimansa","Islas Caiman", "S.A.", "Islas Caiman, Islas Caiman")
                 .tipoCuenta("Empresa")
                 .saldo(BigDecimal.valueOf(1000))
-                .build();
+                .build(registro);
     }
 }
 
--- src/main/java/Sucursal.java
+++ src/main/java/Sucursal.java
@@ -1,36 +1,17 @@
-import java.util.Date;
+
 
 public class Sucursal {
     private String nombre;
     private String direccion;
-    private Date inauguracion;
+    private String inauguracion;
 
-    private InterfaceClientela registro = new RegistroClientela();
+    public InterfaceClientela registro = new RegistroClientela();
 
-    private Sucursal(Builder builder) {
-        this.nombre = builder.nombre;
-        this.direccion = builder.direccion;
-        this.inauguracion = builder.inauguracion;
-        this.registro = builder.registro;
-    }
-
-    public class Builder {
-        private final String nombre;
-        private final String direccion;
-        private final Date inauguracion;
-        private final InterfaceClientela registro;
-
-        public Builder(String nombre, String direccion, Date inauguracion, InterfaceClientela registro) {
-            this.nombre = nombre;
-            this.direccion = direccion;
-            this.inauguracion = inauguracion;
-            this.registro = registro;
-        }
-
-        public Sucursal build() {
-            // WIP:
-            // return ListaSucursales.cargar(new Sucursal(this));
-            return new Sucursal(this);
+    public Sucursal(String nombre, String direccion, String inauguracion, InterfaceClientela registro) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.inauguracion = inauguracion;
+        this.registro = registro;
     }
 
 }
